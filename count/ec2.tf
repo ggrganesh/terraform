f0a52d4a685693d89501fb35f0e5d6dffd903bdc
Ganesh <ganeshgr90@gmail.com>
2025-02-05 10:56:33 +0530

    Add count folder files

---
+++ count/ec2.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "this" {
+  count = length(var.instances)
+  # count = 3
+  ami                    = var.ami_id # this is our devops practice AMI Id 
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = var.instance_type
+  
+  tags = {
+    Name = var.instances[count.index]
+    
+  }
+
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = var.from_port
+    to_port     = var.to_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+tags = var.sg_tags
+
+}
+
+
+# Vaiable priferences 
+
+# 1. Command Line 
+# 2. tf.vars
+# 3. env var
+# 4. default 
+# 6. user prompt 
